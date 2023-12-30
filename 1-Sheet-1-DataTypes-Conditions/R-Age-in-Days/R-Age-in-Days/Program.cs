8e2c5a2da377994f6a299535c9f36d26760ca7ae
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2023-12-31 00:22:25 +0200

    Upload

---
+++ 1-Sheet-1-DataTypes-Conditions/R-Age-in-Days/R-Age-in-Days/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace R_Age_in_Days;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfDaysInput = int.Parse(Console.ReadLine());
+
+        int numberOfYears , numberOfMonth , numberOfDays;
+        numberOfYears = numberOfDaysInput / 365;
+        numberOfMonth = (numberOfDaysInput - (365 * numberOfYears))/30;
+        numberOfDays = (numberOfDaysInput - (365 * numberOfYears) - (30 * numberOfMonth));
+      
+        Console.WriteLine($"{numberOfYears} years");
+        Console.WriteLine($"{numberOfMonth} months");
+        Console.WriteLine($"{numberOfDays} days");
+    }
+}
