11686a47520481f81e32279d9c12d63b28510432
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2023-12-31 11:59:11 +0200

    Upload

---
+++ 2-Sheet-2-Loops/A-1-to-N/A-1-to-N/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace A_1_to_N;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 1 ; i <= number ; i++) { 
+            Console.WriteLine(i);
+        }
+    }
+}
