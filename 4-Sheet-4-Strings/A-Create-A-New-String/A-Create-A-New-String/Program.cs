c475e3d516a23bbd6479c69905b95f57b2f38c1e
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2024-01-03 15:14:03 +0200

    Upload

---
+++ 4-Sheet-4-Strings/A-Create-A-New-String/A-Create-A-New-String/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace A_Create_A_New_String;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstString = Console.ReadLine();
+        string secondString = Console.ReadLine();
+        Console.WriteLine($"{firstString.Length} {secondString.Length}");
+        string concatStrings = firstString + " " + secondString;
+        Console.WriteLine(concatStrings);
+    }
+}
---
+++ 4-Sheet-4-Strings/B-Lets-use-Getline/B-Lets-use-Getline/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace B_Lets_use_Getline;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] afterSplit = input.Split(@"\");
+        string stringAfterSign = afterSplit[0];
+        Console.WriteLine(stringAfterSign);
+    }
+}
