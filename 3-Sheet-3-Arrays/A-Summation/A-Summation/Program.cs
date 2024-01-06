5b098319ab6a1b03222b78602840a7411618a878
Mohamed-Ashraf-99 <mohamedashrafita@gmail.com>
2024-01-06 23:14:37 +0200

    Upload

---
+++ 3-Sheet-3-Arrays/A-Summation/A-Summation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace A_Summation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int size = int.Parse(Console.ReadLine());
+        
+        string[] numbers = Console.ReadLine().Split();
+        
+        long sum = 0;
+        for (int i = 0; i < size; i++)
+        {
+            long number = long.Parse(numbers[i]);
+            sum += number;
+        }
+        
+        long absSum = Math.Abs(sum);
+        Console.WriteLine(absSum);
+    }
+}
